aded555af2fe85f1d93c1cd52539471f68a7046b
Ashok Modi <btmash@gmail.com>
2015-01-28 13:33:53 -0800

    Upgrade newspaper solution pack.

--- sites/all/modules/islandora_contrib/islandora_solution_pack_newspaper/theme/islandora-newspaper-issue.tpl.php
+++ sites/all/modules/islandora_contrib/islandora_solution_pack_newspaper/theme/islandora-newspaper-issue.tpl.php
@@ -8,8 +8,10 @@
   <span class="islandora-newspaper-issue-navigator">
     <?php print theme('islandora_newspaper_issue_navigator', array('object' => $object)); ?>
   </span>
-  <?php if ($viewer_id == 'islandora_internet_archive_bookreader'): ?>
-    <?php print $viewer; ?>
+  <?php if ($viewer_id): ?>
+    <div id="book-viewer">
+      <?php print $viewer; ?>
+    </div>
   <?php else: ?>
     <?php print theme('islandora_objects', array('objects' => $pages)); ?>
   <?php endif; ?>
